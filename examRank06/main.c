54a84e12dcfc330d9eed783b372a87fc4cf7da89
Fargesia01 <slott@student.42lausanne.ch>
2024-01-25 12:04:01 +0100

    finished

--- examRank06/main.c
+++ examRank06/main.c
@@ -14,6 +14,18 @@ typedef struct client_s
     int id;
 }   client_t;
 
+int get_id(client_t *clients, int socket, int nbr_client)
+{
+  int id;
+
+  for (int i = 0; i < nbr_client; i++)
+  {
+    if (clients[i].socket == socket)
+      return (clients[i].id);
+  }
+  return (-1);
+}
+
 int main(int argc, char ** argv)
 {
   if (argc != 2)
@@ -22,10 +34,11 @@ int main(int argc, char ** argv)
     exit(1);
   }
 
-  client_t clients[MAX_CLIENTS];
+  client_t *clients = NULL;
   int next_id = 0;
   int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
   fd_set activeSockets;
+  int nbr_client = 0;
   
   if (serverSocket < 0)
   {
@@ -80,41 +93,63 @@ int main(int argc, char ** argv)
           }
 
           FD_SET(clientSocket, &activeSockets);
+          nbr_client++;
           maxSocket = (clientSocket > maxSocket) ? clientSocket : maxSocket;
-          clients[next_id].socket = clientSocket;
-          clients[next_id].id = next_id;
-          next_id++;
+          client_t *tmp = calloc(nbr_client + 1, sizeof(client_t *));
+          for (int i = 0; i < nbr_client - 1; i++)
+          {
+            tmp[i].id = clients[i].id;
+            tmp[i].socket = clients[i].socket;
+          }
+          tmp[nbr_client - 1].id = next_id;
+          tmp[nbr_client - 1].socket = clientSocket;
+          free(clients);
+          clients = tmp;
 
           sprintf(buffer, "server: client %d just arrived\n", next_id);
-          for (int i = 0; i < next_id; i++)
+          for (int i = 0; i < nbr_client; i++)
           {
             send(clients[i].socket, buffer, strlen(buffer), 0);
           }
+          next_id++;
         }
         else 
         {
           int bytesRead = recv(socketId, buffer, sizeof(buffer) - 1, 0);
-          printf("%s\n", buffer);
+          int id = get_id(clients, socketId, nbr_client);
 
           if (bytesRead <= 0)
           {
-            sprintf(buffer, "server: client %d just left\n", socketId);
+            sprintf(buffer, "server: client %d just left\n", id);
             
-            for (int i = 0; i < next_id; i++)
+            for (int i = 0; i < nbr_client; i++)
             {
               if (clients[i].socket != socketId)
                 send(clients[i].socket, buffer, strlen(buffer), 0);
             }
             close(socketId);
             FD_CLR(socketId, &activeSockets);
+            nbr_client--;
+            client_t *tmp = calloc(nbr_client + 1, sizeof(client_t));
+            for (int i = 0, j = 0; i < nbr_client + 1; i++)
+            {
+              if (clients[i].socket != socketId)
+              {
+                tmp[j].id = clients[i].id;
+                tmp[j].socket = clients[i].socket;
+                j++;
+              }
+            }
+            free(clients);
+            clients = tmp;
           }
           else 
           {
             char message[BUFFER_SIZE];
             bzero(message, BUFFER_SIZE);
-            sprintf(message, "client %d: %s\n", socketId, buffer);
+            sprintf(message, "client %d: %s\n", id, buffer);
 
-            for (int i = 0; i < next_id; i++)
+            for (int i = 0; i < nbr_client; i++)
             {
               if (clients[i].socket != socketId)
                 send(clients[i].socket, message, strlen(message), 0);
