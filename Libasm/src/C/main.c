79fbbcd559040330878be8e091389362a4de1f00
Fargesia01 <slott@student.42lausanne.ch>
2024-05-16 11:07:17 +0200

    modified_main_libasm

--- Libasm/src/C/main.c
+++ Libasm/src/C/main.c
@@ -2,29 +2,29 @@
 #include "bonus.h"
 
 int main() {
-    //// Test ft_strlen
-    //printf("Test ft_strlen\n");
-    //test_ft_strlen();
+    // Test ft_strlen
+    printf("Test ft_strlen\n");
+    test_ft_strlen();
 
-    //// Test ft_strcpy
-    //printf("\nTest ft_strcpy\n");
-    //test_ft_strcpy();
+    // Test ft_strcpy
+    printf("\nTest ft_strcpy\n");
+    test_ft_strcpy();
 
-    ////Test ft_strcmp
-    //printf("\nTest ft_strcmp\n");
-    //test_ft_strcmp();
-    //
-    ////Test ft_write
-    //printf("\nTest ft_write\n");
-    //test_write();
-    //
-    ////Test ft_read
-    //printf("\nTest ft_read\n");
-    //test_read();
+    //Test ft_strcmp
+    printf("\nTest ft_strcmp\n");
+    test_ft_strcmp();
+    
+    //Test ft_write
+    printf("\nTest ft_write\n");
+    test_write();
+    
+    //Test ft_read
+    printf("\nTest ft_read\n");
+    test_read();
 
-    ////Test ft_strdup
-    //printf("\nTest ft_strdup\n");
-    //test_ft_strdup();
+    //Test ft_strdup
+    printf("\nTest ft_strdup\n");
+    test_ft_strdup();
     
     //test();
 
