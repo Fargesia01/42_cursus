3d76c67690f5abd76be9db40037705e7893d685b
Fargesia01 <slott@student.42lausanne.ch>
2025-02-25 13:50:37 +0100

    computor

--- ComputorV1/computorv1.py
+++ ComputorV1/computorv1.py
@@ -14,8 +14,8 @@ x_eq2 = 0
 x2_eq2 = 0
 
 # check if the user has entered an equation
-if len(sys.argv) < 2:
-    print("Not enough arguments")
+if len(sys.argv) != 2:
+    print("Wrong number of arguments")
     sys.exit(1)
 
 # Detects wether the degree is higher
@@ -24,7 +24,7 @@ if ("X^3" in sys.argv[1] or "X^4" in sys.argv[1] or "X^5" in sys.argv[1] or "X^6
     sys.exit(1)
 
 # Detects any invalid characters
-if (any(c not in " *+-=X^0123456789" for c in sys.argv[1])):
+if (any(c not in " .*+-=X^0123456789" for c in sys.argv[1])):
     print("Invalid characters", [c for c in sys.argv[1] if c not in " *+-=X^0123456789"])
     sys.exit(1)
 
@@ -36,47 +36,64 @@ args2 = eq2.split(" ")
 
 # Extracts the coefficients of the left side equation
 for i, str in enumerate(args1):
-    if (len(str) > 3):
+    if (len(str) > 3 and "X^" in str):
         print("The polynomial degree is stricly greater than 2, I can't solve.")
         sys.exit(1)
     if ("X^2" in str):
-        x2_eq1 = float(args1[i-2])
+        tmp = float(args1[i-2])
         if (i >= 3 and args1[i-3] == '-'):
-            x2_eq1 *= -1
+            tmp *= -1
+        x2_eq1 += tmp
     elif ("X^1" in str):
-        x_eq1 = float(args1[i-2])
+        tmp = float(args1[i-2])
         if (i >= 3 and args1[i-3] == '-'):
-            x_eq1 *= -1
+            tmp *= -1
+        x_eq1 += tmp
     elif ("X^0" in str):
-        decimal_eq1 = float(args1[i-2])
+        tmp = float(args1[i-2])
         if (i >= 3 and args1[i-3] == '-'):
-            decimal_eq1 *= -1
+            tmp *= -1
+        decimal_eq1 += tmp
 
 # Extracts the coefficients of the right side equation
 for i, str in enumerate(args2):
-    if (len(str) > 3):
+    if (len(str) > 3 and "X^" in str):
         print("The polynomial degree is stricly greater than 2, I can't solve.")
         sys.exit(1)
     if ("X^2" in str):
-        x2_eq2 = float(args2[i-2])
+        tmp = float(args2[i-2])
         if (i >= 3 and args2[i-3] == '-'):
-            x2_eq2 *= -1
+            tmp *= -1
+        x2_eq2 += tmp
     elif ("X^1" in str):
-        x_eq2 = float(args2[i-2])
+        tmp = float(args2[i-2])
         if (i >= 3 and args2[i-3] == '-'):
-            x_eq2 *= -1
+            tmp *= -1
+        x_eq2 += tmp
     elif ("X^0" in str):
-        decimal_eq2 = float(args2[i-2])
+        tmp = float(args2[i-2])
         if (i >= 3 and args2[i-3] == '-'):
-            decimal_eq2 *= -1
+            tmp *= -1
+        decimal_eq2 += tmp
 
 # Makes the polynomial = 0
 a = x2_eq1 - x2_eq2
 b = x_eq1 - x_eq2
 c = decimal_eq1 - decimal_eq2
 
+if (a == 0 and b == 0 and c == 0):
+    print("Equation in its reduced form: 0 = 0")
+    print("Polynomial degree: 0")
+    print("All real numbers are solutions")
+    sys.exit(0)
+elif (a == 0 and b == 0):
+    print(f"Equation in its reduced form: {c} = 0")
+    print("Polynomial degree: 0")
+    print("There are no solutions")
+    sys.exit(0)
+
 if (a != 0):
-    print("Equation in its reduced form: ", a, "* X^2 +", b, "* X +", c, "= 0")
+    print(f"Equation in its reduced form: {a} * X^2 {'+ ' if b >= 0 else '- '}{abs(b)} * X {'+ ' if c >= 0 else '- '}{abs(c)} = 0")
     print("Polynomial degree: 2")
     solve_2nd_degree(a, b, c)
 elif (b != 0):
